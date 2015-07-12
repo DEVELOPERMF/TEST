5b83c6aaab8bd483d76b0792293b4f69b7983db9
DEVELOPERMF <kamil.sopata.snv@gmail.com>
2015-07-12 23:06:23 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,20 +1,4 @@
 //Tento script je vytvorený MRFALSOM pre TEST účely.
 
-API.sendChat("@MRFALSE> Test script bol spustený!");
-$('#woot').click();
-function advance(obj) {
-	$('#woot').click();
-}
-skipCommand: {
-                command: 'skip',
-                type: 'exact',
-                functionality: function (chat, cmd) {
-                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
-                    else {
-                        API.sendChat("Test Skip!");
-                        API.moderateForceSkip();
-                        
-                    }
-                }
-            }
+API.sendChat("@MRFALSE> Test SKIP SCRIPT je spustený!");
+API.moderateForceSkip();
