8a42cf758caf62b73ce2c96853d954ea7a3dc397
DEVELOPERMF <kamil.sopata.snv@gmail.com>
2015-07-12 22:20:03 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,4 +1,4 @@
 //Tento script je vytvorený MRFALSOM pre TEST účely.
 
-API.sendChat("MRFALSE");
+API.sendChat("@MRFALSE :heart: :purple_heart: :blue_heart: :green_heart: :yellow_heart: :heart: :yellow_heart: :green_heart: :blue_heart: :purple_heart: :heart:");
 $('#woot').click();
