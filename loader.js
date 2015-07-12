c29258c068c85b8ceb1fcd582b286dcd159dfe58
DEVELOPERMF <kamil.sopata.snv@gmail.com>
2015-07-12 22:52:46 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 //Tento script je vytvorený MRFALSOM pre TEST účely.
 
-API.sendChat("@MRFALSE> Test System bol spustený!");
+API.sendChat("@MRFALSE> Test script bol spustený!");
 $('#woot').click();
 function advance(obj) {
 	$('#woot').click();
