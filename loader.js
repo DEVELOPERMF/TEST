a96e5e049dea250d65a4d621c171f6b67d49546d
DEVELOPERMF <kamil.sopata.snv@gmail.com>
2015-07-22 18:05:54 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,6 +1,31 @@
 //Tento script je vytvorený MRFALSOM pre TEST účely.
 
-API.sendChat("/me @MRFALSE> Testovací bot zapnutý.");
+$('#woot').click();
+var emojibuttonon = $(".icon-emoji-on");
+if (emojibuttonon.length > 0) {
+	emojibuttonon[0].click();
+}
+var onlyaudio = $(".item.s-vo");
+if (onlyaudio.length > 0) {
+	onlyaudio[0].click();
+}
+
+var avatarsoff = $(".item.s-dancing.selected");
+if (avatarsoff.length > 0) {
+	avatarsoff[0].click();
+}
+
+var videooff = $(".item.s-av.selected");
+if (videooff.length > 0) {
+	videooff[0].click();
+}
+
+API.sendChat("/cap 1");
+API.chatLog('AutoWoot online! Vytvorený MRFALSE.');
+API.setVolume(0);
+
+API.on(API.CHAT, chat);
+API.on(API.ADVANCE, advance);
 
 function advance(obj) {
 	$('#woot').click();
@@ -10,21 +35,9 @@ function chat(data) {
 	var msg = data.message;
 	var from = data.un;
 
-	if(from === "-WAIN") {
-		if(msg === "!meh") {
-			$('#meh').click();
-		}
-		if(msg === "!woot") {
-			$('#woot').click();
-		}
-	}
-
 	if(from === "MRFALSE") {
-		if(msg === "!meh") {
+		if(msg === "!afk") {
 			$('#meh').click();
-		}
-		if(msg === "!woot") {
-			$('#woot').click();
 		}
 	}
 }
