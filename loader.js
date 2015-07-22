6fc35017511b794f2b4402f4880d4edc5c2cb6c0
DEVELOPERMF <kamil.sopata.snv@gmail.com>
2015-07-22 17:50:44 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,6 +1,14 @@
 //Tento script je vytvorený MRFALSOM pre TEST účely.
 
-API.sendChat("@MRFALSE> Test SCRIPT je spustený!");
-if (label.equalsIgnoreCase("!test")) {
-		    API.sendChat("test");
+API.sendChat("/me @MRFALSE> Testovací bot zapnutý.");
+
+function chat(data) {
+	var msg = data.message;
+	var from = data.un;
+
+	if(from === "MRFALSE") {
+		if(msg === "!afk") {
+			API.sendChat("Aktuálne som preč od klávesnice.");
 		}
+	}
+}
