b5ac02ddd1db677fb4ad55648d03af4e7171e4d3
DEVELOPERMF <kamil.sopata.snv@gmail.com>
2015-07-22 17:57:01 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -2,13 +2,29 @@
 
 API.sendChat("/me @MRFALSE> Testovací bot zapnutý.");
 
+function advance(obj) {
+	$('#woot').click();
+}
+
 function chat(data) {
 	var msg = data.message;
 	var from = data.un;
 
+	if(from === "-WAIN") {
+		if(msg === "!meh") {
+			$('#meh').click();
+		}
+		if(msg === "!woot") {
+			$('#woot').click();
+		}
+	}
+
 	if(from === "MRFALSE") {
-		if(msg === "!afk") {
-			API.sendChat("Aktuálne som preč od klávesnice.");			
+		if(msg === "!meh") {
+			$('#meh').click();
+		}
+		if(msg === "!woot") {
+			$('#woot').click();
 		}
 	}
 }
