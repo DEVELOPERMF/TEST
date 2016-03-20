6e25a987dafcb74f60f8ad58d561284668dc89b5
DEVELOPERMF <kamil.sopata.snv@gmail.com>
2016-03-20 17:07:05 +0100

    Update ban.js

--- ban.js
+++ ban.js
@@ -1,55 +1,47 @@
-//AutoWoot vytvorený THEFALSOM. https://plug.dj/m-f-e
+//SCRIPT vytvorený FALSEYING. https://plug.dj/m-f-e
 
 $('#woot').click();
 var emojibuttonon = $(".icon-emoji-on");
-if (emojibuttonon.length > 0) {
-	emojibuttonon[0].click();
+if (emojibuttonon.length > 0){
+emojibuttonon[0].click();
 }
+
 var onlyaudio = $(".item.s-vo");
-if (onlyaudio.length > 0) {
-	onlyaudio[0].click();
+if (onlyaudio.length > 0){
+onlyaudio[0].click();
 }
 
 var avatarsoff = $(".item.s-dancing.selected");
-if (avatarsoff.length > 0) {
-	avatarsoff[0].click();
+if (avatarsoff.length > 0){
+avatarsoff[0].click();
 }
 
 var videooff = $(".item.s-av.selected");
-if (videooff.length > 0) {
-	videooff[0].click();
+if (videooff.length > 0){
+videooff[0].click();
 }
 
 API.sendChat("/cap 1");
-API.chatLog('AutoWoot online! Vytvorený MRFALSE.');
+API.chatLog('SCRIPT aktivovaný! Vytvorený FALSEYING.');
 API.setVolume(0);
 
 API.on(API.CHAT, chat);
 API.on(API.ADVANCE, advance);
 
-function advance(obj) {
-	$('#woot').click();
+function advance(obj){
+$('#woot').click();
 }
 
-function chat(data) {
-	var msg = data.message;
-	var from = data.un;
+function chat(data){
+var msg = data.message;
+var from = data.un;
 
-	if(from === "-WAIN") {
-		if(msg === "!meh") {
-			$('#meh').click();
-		}
-		if(msg === "!woot") {
-			$('#woot').click();
-		}
-	}
-
-	if(from === "NanoBytez") {
-		if(msg === "!meh") {
-			$('#meh').click();
-		}
-		if(msg === "!woot") {
-			$('#woot').click();
-		}
-	}
+if(from === "FALSEYING | MFE"){
+if(msg === "!ban"){
+API.moderateBanUser(from, 1, API.BAN.HOUR);
+}
+if(msg === "!woot") {
+$('#woot').click();
+}
+}
 }
